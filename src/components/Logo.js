c712a879a58f7844c985c11e98448f2059e71610
신지원 <s420204c@gamil.com>
2024-06-11 21:39:19 +0900

    feat: add logo size customization

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import logo from "../assets/image-logo.svg";
 
-function Logo() {
+export default function Logo({ size }) {
   return (
     <div>
       <Link to="/">
-        <img src={logo} alt="logo" />
+        <LogoImg src={logo} alt="Logo" size={size} />
       </Link>
     </div>
   );
 }
 
-export default Logo;
+const SIZES = {
+  small: { width: 124, height: 49 },
+  medium: { width: 146, height: 57 },
+  large: { width: 248, height: 98 },
+};
+
+const LogoImg = styled.img`
+  width: ${({ size }) => SIZES[size].width ?? SIZES["medium"]}px;
+  height: ${({ size }) => SIZES[size].height ?? SIZES["medium"]}px;
+`;
