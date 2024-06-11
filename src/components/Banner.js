fdfec14eec545b04534bf5061e1d46541329b766
hayuri90 <hayuri90@gmail.com>
2024-06-11 23:56:55 +0900

    📰feat: Implement Banner component with dynamic styling based on props

---
+++ src/components/Banner.js
@@ -0,0 +1,42 @@
+import styled, { css } from "styled-components";
+import banner from "../assets/image-banner.svg";
+
+const LOCATION = {
+  main: { bottom: "0" },
+  other: { top: "0", height: 177 },
+};
+
+const BannerContainer = styled.div`
+  width: 100%;
+  position: fixed;
+  text-align: center;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: -1;
+  overflow: hidden;
+
+  ${({ page }) => css`
+    ${LOCATION[page] || LOCATION.other}
+  `}
+`;
+
+const BannerImg = styled.img`
+  max-width: 100%;
+
+  ${({ page }) =>
+    page !== "main" &&
+    `
+    position: absolute;
+    bottom: 80px;
+    left: 50%;
+    transform: translateX(-50%) translateY(50%);
+  `}
+`;
+
+export default function Banner({ page }) {
+  return (
+    <BannerContainer page={page}>
+      <BannerImg src={banner} page={page} alt="Banner Image" />
+    </BannerContainer>
+  );
+}
