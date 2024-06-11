2aab848890775354a3239e35ef392bf2df5fc77b
KingNono1030 <hoejy0821@gmail.com>
2024-06-11 20:30:54 +0900

    feat: define styles in theme.js

---
+++ src/utils/theme.js
@@ -0,0 +1,63 @@
+export const theme = {
+  spacing: {
+    xxxs: '4px',
+    xxs: '6px',
+    xs: '8px',
+    sm: '12px',
+    md: '16px',
+    lg: '20px',
+    xl: '24px',
+    xxl: '32px',
+    xxxl: '50px',
+  },
+  colors: {
+    blue50: '#1877f2',
+    yellow50: '#fee500',
+    blue50: '#b93333',
+  },
+  grayScale: {
+    gray10: '#ffffff',
+    gray20: '#f9f9f9',
+    gray30: '#cfcfcf',
+    gray40: '#818181',
+    gray50: '#515151',
+    gray60: '#000000',
+  },
+  brownScale: {
+    brown10: '#f5f1ee',
+    brown20: '#e4d5c9',
+    brown30: '#c7bbb5',
+    brown40: '#542f1a',
+    brown50: '#341909',
+  },
+  font: {
+    weight: {
+      regular: 400,
+      bold: 500,
+    },
+    size: {
+      xs: '14px',
+      sm: '16px',
+      md: '18px',
+      lg: '20px',
+      xl: '24px',
+      xxl: '32px',
+      xxxl: '40px',
+    },
+    lineHeight: {
+      xs: '18px',
+      sm: '22px',
+      md: '24px',
+      lg: '25px',
+      xl: '30px',
+      xxl: '40px',
+      xxxl: '48px',
+    },
+    family: 'Pretendard, Arial, sans-serif',
+  },
+  boxShadow: {
+    light: '0px 4px 4px 0px #8C8C8C40',
+    medium: '0px 4px 4px 0px #00000040',
+    heavy: '0px 16px 20px 0px #3030309E',
+  },
+};
