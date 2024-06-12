b5f27b24c1398e91ad85441a4227a9b4175ca72a
JeongYuseung <dbtmd0925@gmail.com>
2024-06-12 18:06:38 +0900

    feat(form): add props to customize icon and placeholder

--- src/components/form.js
+++ src/components/form.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
-import PersonIcon from '../assets/icon-person.svg';
 
-const Form = styled.form`
+
+export default function Form({ onSubmit, state, text, showIcon, iconInstance }) {
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(name);
+  };
+
+  return (
+    <FormWrapper onSubmit={handleSubmit}>
+      <InputContainer>
+        {showIcon && <Icon src={iconInstance} alt="User Icon" />}
+        <Input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder={text}
+        />
+      </InputContainer>
+      <Button type="submit">일단 버튼</Button>
+    </FormWrapper>
+  );
+}
+
+const FormWrapper = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 305px;
-  height: Hug (156px)px;
-  top: 260px;
-  left: 35px;
+  height: Hug (156px);
   padding: 24px;
   gap: 16px;
   border-radius: 16px;
-  opacity: 0px;
   background-color: white;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
@@ -38,14 +59,12 @@ const Input = styled.input`
   font-weight: 400;
   text-align: left;
   width: 257px;
-  height: Hug (46px);
+  height: 22px;
   padding: 10px 10px 10px 40px;
   border: 1px solid #818181;
   border-radius: 5px;
   font-size: 16px;
   line-height: 22px;
-  
-
 
   &::placeholder {
     color: rgba(0, 0, 0, 0.5);
@@ -55,31 +74,4 @@ const Input = styled.input`
 const Button = styled.button`
   width: 100%;
   padding: 10px 20px;
-
 `;
-
-const NameForm = ({ onSubmit }) => {
-  const [name, setName] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(name);
-  };
-
-  return (
-    <Form onSubmit={handleSubmit}>
-      <InputContainer>
-        <Icon src={PersonIcon} alt="User Icon" />
-        <Input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder={"이름을 입력하세요"}
-        />
-      </InputContainer>
-      <Button type="submit">일단 버튼</Button> {}
-    </Form>
-  );
-};
-
-export default NameForm;
