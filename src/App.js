b69d17ad9071f5bae0fe38c6e98e5f72e3ce7731
신지원 <s420204c@gamil.com>
2024-06-12 17:47:08 +0900

    style: changed double quotes to single quotes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import LandingPage from "./pages/LandingPage";
-import QuestionListPage from "./pages/QuestionListPage";
-import QuestionFeedPage from "./pages/QuestionFeedPage";
-import AnswerFeedPage from "./pages/AnswerFeedPage";
-import { theme } from "./utils/theme";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import QuestionListPage from './pages/QuestionListPage';
+import QuestionFeedPage from './pages/QuestionFeedPage';
+import AnswerFeedPage from './pages/AnswerFeedPage';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<LandingPage />} />
-            <Route path="list">
-              <Route index element={<QuestionListPage />} />
-            </Route>
-            <Route path="post">
-              <Route path=":postId">
-                <Route index element={<QuestionFeedPage />} />
-                <Route path="answer" element={<AnswerFeedPage />} />
-              </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<LandingPage />} />
+          <Route path='list'>
+            <Route index element={<QuestionListPage />} />
+          </Route>
+          <Route path='post'>
+            <Route path=':postId'>
+              <Route index element={<QuestionFeedPage />} />
+              <Route path='answer' element={<AnswerFeedPage />} />
             </Route>
           </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,13 +1,13 @@
-import styled from "styled-components";
-import profile from "../assets/image-profile.svg";
-import messages from "../assets/icon-messages.svg";
+import styled from 'styled-components';
+import profileImage from '../assets/image-profile.svg';
+import messageIcon from '../assets/icon-messages.svg';
 
 export default function Feed() {
   return (
     <ul>
-      <StyledProfile src={profile} alt="피드 프로필" />
+      <StyledProfile src={profile} alt='피드 프로필' />
       <li>아초는고양이</li>
-      <StyledMessages src={messages} alt="피드 메시지 아이콘" />
+      <StyledMessages src={messages} alt='피드 메시지 아이콘' />
       <li>받은 질문</li>
       <li>9개</li>
     </ul>
