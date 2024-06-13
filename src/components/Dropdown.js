895d9a578a90cade939ed7c81f10fe04fdaf6d67
KingNono1030 <hoejy0821@gmail.com>
2024-06-13 22:08:49 +0900

    feat: add icon in dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import arrowUpIcon from '../assets/icon-arrow-up.svg';
+import arrowDownIcon from '../assets/icon-arrow-down.svg';
 
 const options = ['이름순', '최신순'];
 
 export default function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
   const [value, setValue] = useState(options[0]);
+  const arrow = isOpen ? arrowUpIcon : arrowDownIcon;
   return (
     <DropdownWrapper>
       <DropdownButton
@@ -15,6 +18,7 @@ export default function Dropdown() {
       >
         {value}
       </DropdownButton>
+      <ArrowIcon src={arrow} alt='드롭다운 화살표 아이콘' />
       {isOpen && (
         <OptionList>
           {options.map((option) => (
@@ -30,5 +34,6 @@ export default function Dropdown() {
 
 const DropdownWrapper = styled.div``;
 const DropdownButton = styled.button``;
+const ArrowIcon = styled.img``;
 const OptionList = styled.ul``;
 const Option = styled.li``;
