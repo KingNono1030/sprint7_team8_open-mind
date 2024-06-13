0b30260260f1eed0bc044cfa2c226047fde5974b
KingNono1030 <hoejy0821@gmail.com>
2024-06-13 18:56:28 +0900

    feat: add get methods in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 const API_BASE_URL = 'https://openmind-api.vercel.app';
 const TEAM = '7-8';
 
-export async function createSubject(subjectName) {
+export async function createFeed(feedName = '') {
   const FormData = {
-    name: { subjectName },
+    name: { feedName },
     team: { TEAM },
   };
   const response = await fetch(`${API_BASE_URL}/${TEAM}/subjects`, {
@@ -33,6 +33,21 @@ export async function createQuestions(FormData) {
   return body;
 }
 
+export async function createReaction(questionId = '', FormData) {
+  const response = await fetch(
+    `${API_BASE_URL}/${TEAM}/questions/${questionId}/reaction`,
+    {
+      method: 'POST',
+      body: FormData,
+    }
+  );
+  if (!response.ok) {
+    throw new Error('반응을 보내는데 실패했습니다');
+  }
+  const body = await response.json();
+  return body;
+}
+
 export async function createAnswers(FormData) {
   const { questionId } = FormData;
   const response = await fetch(
@@ -49,23 +64,54 @@ export async function createAnswers(FormData) {
   return body;
 }
 
-export async function createReaction(questionId, FormData) {
+export async function getFeedList({ limit = 0, offset = 0 }) {
   const response = await fetch(
-    `${API_BASE_URL}/${TEAM}/questions/${questionId}/reaction`,
-    {
-      method: 'POST',
-      body: FormData,
-    }
+    `${API_BASE_URL}/${TEAM}/subjects/?limit=${limit}&offset=${offset}`
   );
   if (!response.ok) {
-    throw new Error('반응을 보내는데 실패했습니다');
+    throw new Error('피드 리스트를 불러오는데 실패했습니다');
   }
   const body = await response.json();
   return body;
 }
 
-export async function getAnswers(id) {
-  const response = await fetch(`${API_BASE_URL}/${TEAM}/${id}`);
+export async function getFeed(subjectId = '') {
+  const response = await fetch(`${API_BASE_URL}/${TEAM}/subjects/${subjectId}`);
+  if (!response.ok) {
+    throw new Error('피드를 불러오는데 실패했습니다');
+  }
+  const body = await response.json();
+  return body;
+}
+
+export async function getQuestionList({
+  subjectId = '',
+  limit = 0,
+  offset = 0,
+}) {
+  const response = await fetch(
+    `${API_BASE_URL}/${TEAM}/subjects/${subjectId}/questions/?limit=${limit}&offset=${offset}`
+  );
+  if (!response.ok) {
+    throw new Error('질문 리스트를 불러오는데 실패했습니다');
+  }
+  const body = await response.json();
+  return body;
+}
+
+export async function getQuestion(questionId = '') {
+  const response = await fetch(
+    `${API_BASE_URL}/${TEAM}/questions/${questionId}`
+  );
+  if (!response.ok) {
+    throw new Error('질문을 불러오는데 실패했습니다');
+  }
+  const body = await response.json();
+  return body;
+}
+
+export async function getAnswer(answerId = '') {
+  const response = await fetch(`${API_BASE_URL}/${TEAM}/${answerId}`);
   if (!response.ok) {
     throw new Error('답변을 불러오는데 실패했습니다');
   }
