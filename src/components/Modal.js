7ebd31df2bf4744be87745db80bfcda8e0abcee3
cossgii <eastar385@naver.com>
2024-06-13 15:36:47 +0900

    Feat: Add Style

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,32 +1,88 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Profile from './Profile';
+import CloseIcon from 'srcassetsicon-close.svg';
+import MessageIcon from 'srcassetsicon-messages.svg';
 
-const H2 = styled.h2`
-  width: 532px;
-  height: 30px;
-  display: flex;
-  justify-content: space-between;
-`;
-const H2L = styled(H2)`
-  width: 219px;
-`;
 function Modal() {
   return (
     <>
-      <H2>
-        <H2L>
-          <img src="src\assets\icon-messages.svg" />
-          <span>질문을 작성해주세요</span>
-        </H2L>
-        <img src="src\assets\icon-close.svg" />
-      </H2>
-      <div className="Receiver">
-        <text>To.</text>
-        <img />
-        <text>아초는고양이</text>
-      </div>
-      <form>{/*질문 내용 작성 폼 */}</form>
+      <ModalHeader />
+      <Receiver />
+      <QuestionForm />
     </>
   );
 }
+
+function ModalHeader() {
+  return (
+    <>
+      <ModalDescription />
+      <button>
+        <img src={CloseIcon} />
+      </button>
+    </>
+  );
+}
+function Receiver() {
+  return (
+    <>
+      <span>TO.</span>
+      <Profile />
+    </>
+  );
+}
+function ModalDescription() {
+  return (
+    <>
+      <img src={MessageIcon} />
+      <span>질문을 작성하세요</span>
+    </>
+  );
+}
+
+/*
+const FlexContainer = styled.div`
+  width: 327px;
+  height: 568px;
+  top: 122px;
+  left: 23px;
+  gap: 0px;
+  border-radius: 24px 0px 0px 0px;
+  opacity: 0px;
+`
+*/
+
+const ModalHeader = styled.div`
+  width: Fixed (279px) px;
+  height: Hug (28px) px;
+  top: 146px;
+  left: 47px;
+  gap: 0px;
+  justify: space-between;
+  opacity: 0px;
+`;
+
+const ModalDescription = styled.div`
+  width: 188px;
+`;
+
+const Receiver = styled.div`
+  width: 142px;
+  height: 28px;
+  top: 214px;
+  left: 47px;
+  gap: 0px;
+  opacity: 0px;
+`;
+
+const QuestionForm = styled.form`
+  width: Fixed (279px) px;
+  height: Hug (412px) px;
+  top: 254px;
+  left: 47px;
+  gap: 8px;
+  opacity: 0px;
+`;
+
 export default Modal;
