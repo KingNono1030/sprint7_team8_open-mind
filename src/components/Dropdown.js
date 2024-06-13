3c4d03494ae3529e0bbef6fb8036ec9d56b8a3b3
KingNono1030 <hoejy0821@gmail.com>
2024-06-13 23:06:48 +0900

    feat: add additional styles to dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import arrowUpIcon from '../assets/icon-arrow-up.svg';
 import arrowDownIcon from '../assets/icon-arrow-down.svg';
 
@@ -12,6 +12,7 @@ export default function Dropdown() {
   return (
     <DropdownWrapper>
       <DropdownButton
+        isOpen={isOpen}
         onClick={() => {
           setIsOpen(!isOpen);
         }}
@@ -33,19 +34,41 @@ export default function Dropdown() {
   );
 }
 
-const DropdownWrapper = styled.div`
-  position: relative;
+const dropdownButtonColor = css`
+  border: ${({ isOpen, theme }) =>
+    isOpen
+      ? `${theme.border.thin} solid ${theme.grayScale.gray60}`
+      : `${theme.border.thin} solid ${theme.grayScale.gray40}`};
+  color: ${({ isOpen, theme }) =>
+    isOpen ? theme.grayScale.gray60 : theme.grayScale.gray40};
 `;
-const DropdownButton = styled.button`
+
+const buttonContentLayout = css`
   display: flex;
   align-items: center;
+  padding-top: 8px;
+  padding-bottom: 8px;
   padding-left: 12px;
 `;
+
+const DropdownWrapper = styled.div`
+  position: relative;
+  font-weight: ${({ theme }) => theme.font.weight.medium};
+  font-size: ${({ theme }) => theme.font.size.xs};
+  line-height: ${({ theme }) => theme.font.lineHeight.xs};
+`;
+
+const DropdownButton = styled.button`
+  ${buttonContentLayout}
+  ${DropdownButtonColor}
+`;
+
 const ArrowIcon = styled.img`
   position: absolute;
   right: 12px;
   top: 10px;
 `;
+
 const OptionList = styled.ul`
   display: flex;
   flex-direction: column;
@@ -53,10 +76,13 @@ const OptionList = styled.ul`
   position: absolute;
   top: calc(100% + 4px);
   padding: 4px 0;
+  border: ${({ theme }) =>
+    `${theme.border.thin} solid ${theme.grayScale.gray30}`};
+  color: ${({ theme }) => theme.grayScale.gray50};
   z-index: 1;
 `;
+
 const Option = styled.li`
-  display: flex;
-  align-items: center;
-  padding-left: 12px;
+  ${buttonContentLayout}
+  cursor: pointer;
 `;
