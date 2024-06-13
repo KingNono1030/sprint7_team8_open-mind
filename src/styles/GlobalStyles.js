baa4b6f9250026b8124533150344d32262ecd0ab
KingNono1030 <hoejy0821@gmail.com>
2024-06-13 21:21:58 +0900

    feat: add font and box-sizing in global styles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,7 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+    * {
+    box-sizing: border-box;
+  }
 
+  body {
+    font-family: ${({ theme }) => theme.font.family};
+  }
 `;
 
 export default GlobalStyles;
