7202df497b5352d073899721ff41cd1d42843220
hayuri90 <hayuri90@gmail.com>
2024-06-15 02:44:34 +0900

    ✨feat: Add toggle, styling in Reaction.js

--- src/components/Reaction.js
+++ src/components/Reaction.js
@@ -1,15 +1,96 @@
-import thumbUpIcon from '../assets/icon-thumbs-up.svg';
-import ThumbUpActiveIcon from '../assets/icon-thumbs-up--blue.svg';
-import thumbDownIcon from '../assets/icon-thumbs-down.svg';
-import ThumbDownActiveIcon from '../assets/icon-thumbs-down--black.svg';
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
+import thumbsUpIcon from '../assets/icon-thumbs-up.svg';
+import thumbsUpActiveIcon from '../assets/icon-thumbs-up--blue.svg';
+import thumbsDownIcon from '../assets/icon-thumbs-down.svg';
+import thumbsDownActiveIcon from '../assets/icon-thumbs-down--black.svg';
 
 export default function Reaction() {
+  const [thumbsUpActive, setThumbsUpActive] = useState(false);
+  const [thumbsDownActive, setThumbsDownActive] = useState(false);
+
+  const handleReactionClick = (type) => {
+    if (type === 'like') {
+      setThumbsUpActive(!thumbsUpActive);
+      if (thumbsDownActive) {
+        setThumbsDownActive(false);
+      }
+    } else if (type === 'dislike') {
+      setThumbsDownActive(!thumbsDownActive);
+      if (thumbsUpActive) {
+        setThumbsUpActive(false);
+      }
+    }
+  };
+
+  const reactions = [
+    {
+      icon: thumbsUpActive ? thumbsUpActiveIcon : thumbsUpIcon,
+      text: '좋아요',
+      onClick: () => handleReactionClick('like'),
+      active: thumbsUpActive,
+      defaultColor: '#818181',
+      activeColor: '#1877F2',
+    },
+    {
+      icon: thumbsDownActive ? thumbsDownActiveIcon : thumbsDownIcon,
+      text: '싫어요',
+      onClick: () => handleReactionClick('dislike'),
+      active: thumbsDownActive,
+      defaultColor: '#818181',
+      activeColor: '#000000',
+    },
+  ];
+
   return (
-    <div>
-      <img src={thumbUpIcon} />
-      <img src={ThumbUpActiveIcon} />
-      <img src={thumbDownIcon} />
-      <img src={ThumbDownActiveIcon} />
-    </div>
+    <ReactionContainer>
+      {reactions.map((reaction) => (
+        <ReactionBtn onClick={reaction.onClick} active={reaction.active}>
+          <img src={reaction.icon} alt={`${reaction.text} 아이콘`} />
+          <span
+            style={{
+              color: reaction.active
+                ? reaction.activeColor
+                : reaction.defaultColor,
+            }}
+          >
+            {`${reaction.text} ${
+              reaction.likeCount !== undefined ? ` ${reaction.likeCount}` : ''
+            }`}
+          </span>
+        </ReactionBtn>
+      ))}
+    </ReactionContainer>
   );
 }
+
+const ReactionContainer = styled.div`
+  width: 168px;
+  height: 18px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 32px;
+`;
+
+const ReactionBtn = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: 0;
+
+  img {
+    width: 14.12px;
+    height: 14.33px;
+  }
+
+  ${({ theme }) => css`
+    span {
+      font-family: ${theme.font.family};
+      font-weight: ${theme.font.weight.medium};
+    }
+  `}
+`;
