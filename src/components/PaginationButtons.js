5640a45eef5d3e01f19f53654aed565752f814b8
KingNono1030 <hoejy0821@gmail.com>
2024-06-15 03:49:12 +0900

    feat: add initial structure of pagination buttons

---
+++ src/components/PaginationButtons.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import arrowLeftIcon from '../assets/icon-arrow-left.svg';
+import arrowRightIcon from '../assets/icon-arrow-right.svg';
+
+const PAGES = [1, 2, 3, 4, 5];
+const getAltMessage = (direction) => `${direction}으로 페이지 이동`;
+
+export default function PaginationButtons() {
+  return (
+    <S.ButtonList>
+      <S.PageButton>
+        <img src={arrowLeftIcon} alt={getAltMessage('왼쪽')} />
+      </S.PageButton>
+      {PAGES.map((page) => {
+        return (
+          <S.PageButton key={page} value={page}>
+            {page}
+          </S.PageButton>
+        );
+      })}
+      <S.PageButton>
+        <img src={arrowRightIcon} alt={getAltMessage('오른쪽')} />
+      </S.PageButton>
+    </S.ButtonList>
+  );
+}
+
+const S = {};
+
+S.ButtonList = styled.ul``;
+S.PageButton = styled.li``;
