ea59b699239cb02a3ef87a797d220be52845460a
KingNono1030 <hoejy0821@gmail.com>
2024-06-14 21:29:31 +0900

    refactor: apply S Dot naming in styled components

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -11,25 +11,25 @@ export default function Dropdown() {
   const [selectedOption, selectOption] = useDropdown(options);
   const arrow = isOpen ? arrowUpIcon : arrowDownIcon;
   return (
-    <DropdownWrapper>
-      <DropdownButton isOpen={isOpen} onClick={toggleDropdown} type='button'>
+    <S.DropdownWrapper>
+      <S.DropdownButton isOpen={isOpen} onClick={toggleDropdown} type='button'>
         {selectedOption}
-      </DropdownButton>
-      <ArrowIcon src={arrow} alt='드롭다운 화살표 아이콘' />
+      </S.DropdownButton>
+      <S.ArrowIcon src={arrow} alt='드롭다운 화살표 아이콘' />
       {isOpen && (
-        <OptionList>
+        <S.OptionList>
           {options.map((option) => (
-            <Option
+            <S.Option
               key={option}
               isSelected={option === selectedOption}
               onClick={selectOption}
             >
               {option}
-            </Option>
+            </S.Option>
           ))}
-        </OptionList>
+        </S.OptionList>
       )}
-    </DropdownWrapper>
+    </S.DropdownWrapper>
   );
 }
 
@@ -60,25 +60,27 @@ const selectedOptionColor = css`
   `};
 `;
 
-const DropdownWrapper = styled.div`
+const s = {};
+
+S.DropdownWrapper = styled.div`
   position: relative;
   font-weight: ${({ theme }) => theme.font.weight.medium};
   font-size: ${({ theme }) => theme.font.size.xs};
   line-height: ${({ theme }) => theme.font.lineHeight.xs};
 `;
 
-const DropdownButton = styled.button`
+S.DropdownButton = styled.button`
   ${buttonContentLayout}
   ${DropdownButtonColor}
 `;
 
-const ArrowIcon = styled.img`
+S.ArrowIcon = styled.img`
   position: absolute;
   right: 12px;
   top: 10px;
 `;
 
-const OptionList = styled.ul`
+S.OptionList = styled.ul`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -91,7 +93,7 @@ const OptionList = styled.ul`
   z-index: 1;
 `;
 
-const Option = styled.li`
+S.Option = styled.li`
   ${buttonContentLayout}
   ${selectedOptionColor}
   cursor: pointer;
