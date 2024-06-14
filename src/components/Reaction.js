6342c4a7972919c89fcf7da54599f6962add615c
hayuri90 <hayuri90@gmail.com>
2024-06-14 16:26:40 +0900

    ✨ feat: Add Reaction icons to Reaction.js

--- src/components/Reaction.js
+++ src/components/Reaction.js
@@ -1,3 +1,8 @@
+import thumbsUpIcon from '../assets/icon-thumbs-up.svg';
+import ThumbUpActiveIcon from '../assets/icon-thumbs-up--blue.svg';
+import thumbsDownIcon from '../assets/icon-thumbs-down.svg';
+import ThumbDownActiveIcon from '../assets/icon-thumbs-down--gray.svg';
+
 export default function Reaction() {
   return <div>Reaction component</div>;
 }
