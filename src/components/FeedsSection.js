1e8a29bb7155b4fd2e9782f59d5b6bcce8505fd6
신지원 <s420204c@gamil.com>
2024-06-15 12:19:30 +0900

    refactor: delete unused code and pagenation

--- src/components/FeedsSection.js
+++ src/components/FeedsSection.js
@@ -7,10 +7,9 @@ export default function FeedsSection() {
     <S.SectionContainer>
       <S.AskContainer>
         <S.AskQuestion>누구에게 질문할까요?</S.AskQuestion>
-        <S.TestButton>드롭다운</S.TestButton>
+        <Dropdown />
       </S.AskContainer>
       <FeedList />
-      <S.Pagenation>페이지네이션</S.Pagenation>
     </S.SectionContainer>
   );
 }
@@ -18,7 +17,6 @@ export default function FeedsSection() {
 const S = {};
 
 S.SectionContainer = styled.div`
-  width: 360px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -32,22 +30,10 @@ S.AskContainer = styled.div`
 `;
 
 S.AskQuestion = styled.h2`
-  margin: 0;
   padding: 2px 42px 2px 0;
   font-family: ${theme.font.family};
   font-size: ${theme.font.size.xl};
   font-weight: ${theme.font.weight.normal};
+  line-height: ${theme.font.lineHeight.xl};
   color: ${theme.grayScale.gray60};
-`;
-
-S.TestButton = styled.div`
-  padding-right: 24px;
-`;
-
-S.Pagenation = styled.div`
-  width: 280px;
-  height: 40px;
-  margin-top: 7px;
-  margin-left: 49px;
-  background-color: red;
 `;
