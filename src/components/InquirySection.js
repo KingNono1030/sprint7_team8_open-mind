8e6a222c4e370607a04472c7bff988cb721a25b4
신지원 <s420204c@gamil.com>
2024-06-16 23:15:58 +0900

    feat: update new styled and emptyImage position

--- src/components/InquirySection.js
+++ src/components/InquirySection.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
+import { theme } from '../utils/theme';
 import styled from 'styled-components';
 import emptyImage from '../assets/Image-empty-inquiry.svg';
 import messageIcon from '../assets/icon-message-brown.svg';
-import { theme } from '../utils/theme';
 
 export default function InquirySection() {
   const [questionCount, setQuestionCount] = useState(3);
@@ -11,11 +11,11 @@ export default function InquirySection() {
     <S.InquiryWrapper>
       <S.SectionHeader>
         <S.MessageIcon src={messageIcon} alt='질문창 메시지 아이콘' />
-        <span>
+        <S.QuestionNumber>
           {questionCount > 0
             ? `${questionCount}개의 질문이 있습니다.`
             : `아직 질문이 없습니다.`}
-        </span>
+        </S.QuestionNumber>
       </S.SectionHeader>
       {questionCount === 0 && (
         <S.EmptyImage src={emptyImage} alt='질문 없을때 빈 상자 아이콘' />
@@ -29,15 +29,16 @@ const S = {};
 S.InquiryWrapper = styled.div`
   display: flex;
   justify-content: center;
+  position: relative;
   width: 327px;
-  height: 500px;
+  height: 330px;
   border: 1px solid ${theme.brownScale.brown20};
   border-radius: ${theme.rounded.md};
+  background-color: ${theme.brownScale.brown10};
 `;
 
 S.SectionHeader = styled.div`
   display: flex;
-  justify-content: center;
   padding: 16px;
 `;
 
@@ -47,7 +48,16 @@ S.MessageIcon = styled.img`
   padding: 1px 8px 1px 0;
 `;
 
+S.QuestionNumber = styled.span`
+  font-family: ${theme.font.family};
+  font-size: ${theme.font.size.md};
+  line-height: ${theme.font.lineHeight.md};
+  color: ${theme.brownScale.brown40};
+`;
+
 S.EmptyImage = styled.img`
-  width: 150px;
-  height: 154px;
+  position: absolute;
+  top: 106px;
+  width: 114px;
+  height: 118px;
 `;
