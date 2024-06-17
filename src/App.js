f8d6b6a65264e6b6375251df0cb08325436154c8
신지원 <s420204c@gamil.com>
2024-06-17 21:59:29 +0900

    refactor: reanaming and change single quote App file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import QuestionListPage from "./pages/QuestionListPage";
-import QuestionFeedPage from "./pages/QuestionFeedPage";
-import AnswerFeedPage from "./pages/AnswerFeedPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import QuestionListPage from './pages/QuestionListPage';
+import QuestionFeedPage from './pages/QuestionFeedPage';
+import AnswerFeedPage from './pages/AnswerFeedPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+        <Route path='/'>
           <Route index element={<LandingPage />} />
-          <Route path="list">
+          <Route path='list'>
             <Route index element={<QuestionListPage />} />
           </Route>
-          <Route path="post">
-            <Route path=":postId">
+          <Route path='post'>
+            <Route path=':postId'>
               <Route index element={<QuestionFeedPage />} />
-              <Route path="answer" element={<AnswerFeedPage />} />
+              <Route path='answer' element={<AnswerFeedPage />} />
             </Route>
           </Route>
         </Route>
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,21 +1,23 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import logo from "../assets/image-logo.svg";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import logoImg from '../assets/image-logo.svg';
 
-export default function Logo({ size = "md" }) {
+export default function Logo({ size = 'md' }) {
   return (
-    <Link to="/">
-      <StyledLogo size={size} src={logo} alt="오픈마인드 로고" />
+    <Link to='/'>
+      <S.LogoImg size={size} src={logoImg} alt='오픈마인드 로고' />
     </Link>
   );
 }
 
+const S = {};
+
 const SIZES = {
-  sm: "124px",
-  md: "146px",
-  lg: "248px",
+  sm: '124px',
+  md: '146px',
+  lg: '248px',
 };
 
-const StyledLogo = styled.img`
+S.LogoImg = styled.img`
   width: ${({ size }) => SIZES[size]};
 `;
