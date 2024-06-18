79c7e7da132f0cb883aab550fa29499d8d70cee3
JeongYuseung <dbtmd0925@gmail.com>
2024-06-18 18:30:49 +0900

    refactor: Update styling method for AnswerStateBadge component

--- src/components/AnswerStateBadge.js
+++ src/components/AnswerStateBadge.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default function AnswerStateBadge({ isAnswered }) {
     return (
-      <AnsweredBadge answered={isAnswered}>
+      <S.AnswerStateBadge answered={isAnswered}>
         {isAnswered ? '답변 완료' : '미답변'}
-      </AnsweredBadge>
+      </S.AnswerStateBadge>
     );
-  }
+}
 
-const AnsweredBadge = styled.div`
-  display: inline-block;
-  padding: 4px 12px;
-  border-radius: 8px;
-  font-size: 14px;
-  font-weight: 500;
-  color: ${({ answered }) => (answered ? '#542F1A' : '#818181')};
-  background-color: ${({ answered }) => (answered ? '#FFFFFF' : '#FFFFFF')};
-  border: 1px solid ${({ answered }) => (answered ? '#542F1A' : '#818181')};
-  `;
+const S = {
+  AnswerStateBadge: styled.div`
+    display: inline-block;
+    padding: 4px 12px;
+    border-radius: 8px;
+    font-size: 14px;
+    font-weight: 500;
+    ${({ answered }) => (answered ? answeredStyles : notAnsweredStyles)}
+  `,
+};
+
+const answeredStyles = css`
+  color: #542F1A;
+  background-color: #FFFFFF;
+  border: 1px solid #542F1A;
+`;
+
+const notAnsweredStyles = css`
+  color: #818181;
+  background-color: #FFFFFF;
+  border: 1px solid #818181;
+`;
