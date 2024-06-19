9260b9bda2bb3652aeacce670a15aed688ebc84f
KingNono1030 <hoejy0821@gmail.com>
2024-06-19 14:05:19 +0900

    feat: add pagination functions

--- src/components/PaginationButtons.js
+++ src/components/PaginationButtons.js
@@ -1,22 +1,38 @@
+import { useState } from 'react';
 import styled, { css } from 'styled-components';
 import { ReactComponent as CaretLeftIcon } from '../assets/icon-caret-left.svg';
 import { ReactComponent as CaretRightIcon } from '../assets/icon-caret-right.svg';
 
-const totalPages = 5; // 원하는 총 페이지 수
-const PAGES = Array.from({ length: totalPages }, (_, index) => index + 1);
+const INITIAL_PAGE = 1;
+const TOTAL_PAGES = 5;
+const PAGES = Array.from({ length: TOTAL_PAGES }, (_, index) => index + 1);
 
 export default function PaginationButtons({ onClick }) {
+  const [page, setPage] = useState(INITIAL_PAGE);
+
+  const onPagination = (e) => {
+    setPage(e.target.innerText);
+  };
+  const onPreviousPage = () => {
+    const isPageOutOfBounds = page <= 1;
+    isPageOutOfBounds || setPage((prevPage) => prevPage - 1);
+  };
+  const onNextPage = () => {
+    const isPageOutOfBounds = page >= 5;
+    isPageOutOfBounds || setPage((prevPage) => prevPage + 1);
+  };
+
   return (
     <S.ButtonList>
-      <S.PageButton>
+      <S.PageButton onClick={onPreviousPage}>
         <CaretLeftIcon />
       </S.PageButton>
       {PAGES.map((page) => (
-        <S.PageButton onClick={onClick} key={page}>
+        <S.PageButton onClick={onPagination} key={page}>
           {page}
         </S.PageButton>
       ))}
-      <S.PageButton>
+      <S.PageButton onClick={onNextPage}>
         <CaretRightIcon />
       </S.PageButton>
     </S.ButtonList>
@@ -27,6 +43,10 @@ const fontStyle = css`
   font-size: ${({ theme }) => theme.font.size.lg};
   line-height: ${({ theme }) => theme.font.lineHeight.lg};
   color: ${({ theme }) => theme.grayScale.gray40};
+`;
+
+const selectedButtonStyles = css`
+  color: ${({ theme }) => theme.brownScale.brown40};
 `;
 
 const S = {};
