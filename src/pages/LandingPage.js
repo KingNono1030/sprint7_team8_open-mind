846b10a46c5a25208391879c67a43c5424039c41
hayuri90 <hayuri90@gmail.com>
2024-06-19 16:54:00 +0900

    feat: adjust layout based on screen size add Link tag

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,21 +1,46 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import Logo from '../components/Logo';
 import Form from '../components/form';
 import personIcon from '../assets/icon-person.svg';
 import QuestionBtn from '../components/Button';
 import Banner from '../components/Banner';
-import styled from 'styled-components';
 
 export default function LandingPage() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const getLogoSize = () => {
+    if (windowWidth >= 768) {
+      return 'xl';
+    } else {
+      return 'lg';
+    }
+  };
+
   return (
     <>
       <S.LandingContainer>
         <S.LogoContainer>
-          <Logo size='lg' />
+          <Logo size={getLogoSize()} />
         </S.LogoContainer>
         <S.AskQuestionBtn>
-          <QuestionBtn isDark={false} hasArrow={true} isArrowRight={true}>
-            질문하러 가기
-          </QuestionBtn>
+          <S.Link to='/list'>
+            <QuestionBtn isDark={false} hasArrow={true} isArrowRight={true}>
+              질문하러 가기
+            </QuestionBtn>
+          </S.Link>
         </S.AskQuestionBtn>
         <S.FormContainer>
           <Form
@@ -45,11 +70,30 @@ S.LandingContainer = styled.div`
 S.LogoContainer = styled.div`
   position: absolute;
   top: 80px;
+
+  @media screen and (min-width: 768px) {
+    top: 160px;
+  }
 `;
 
 S.AskQuestionBtn = styled.div`
   position: absolute;
   top: 202px;
+  text-decoration: none;
+
+  @media screen and (min-width: 768px) {
+    top: 44px;
+    left: 557px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    top: 45px;
+    left: 909px;
+  }
+`;
+
+S.Link = styled(Link)`
+  text-decoration: none;
 `;
 
 S.FormContainer = styled.div`
@@ -61,4 +105,9 @@ S.FormContainer = styled.div`
   padding: 24px;
   gap: 16px;
   border-radius: 16px;
+
+  @media screen and (min-width: 768px) {
+    width: 336px;
+    top: 364px;
+  }
 `;
