1d6349425018a1323e068360d9a3047822fa9234
KingNono1030 <hoejy0821@gmail.com>
2024-06-22 05:27:11 +0900

    feat: add useForm hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const useForm = (initialValues) => {
+  const [value, setValue] = useState(initialValue);
+  const handleChange = (e) => {
+    const nextValue = e.target.value;
+    setValue((prevValue) => nextValue);
+  };
+
+  const handleSubmit = (callback) => (e) => {
+    e.preventDefault();
+    callback(value);
+  };
+
+  return {
+    value,
+    handleChange,
+    handleSubmit,
+  };
+};
