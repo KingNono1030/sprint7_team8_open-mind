ca28576ab2265202a9b4f71a0f753a9513376156
신지원 <s420204c@gamil.com>
2024-06-22 00:01:26 +0900

    refactor: edit layout modal component and delete width in questionForm component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,12 @@
 import { useRef } from 'react';
 import styled from 'styled-components';
-import CloseIcon from '../assets/icon-close.svg';
-import MessageIcon from '../assets/icon-messages.svg';
+import QuestionForm from './QuestionForm';
+import Button from './Button';
+import closeIcon from '../assets/icon-close.svg';
+import messageIcon from '../assets/icon-messages.svg';
+import profileIcon from '../assets/image-default-profile.svg';
 
-function Modal({ onClose }) {
+export default function Modal({ onClose }) {
   const outside = useRef();
 
   const handleWrapperClick = (e) => {
@@ -13,44 +16,37 @@ function Modal({ onClose }) {
   };
 
   return (
-    <ModalWrapper onClick={handleWrapperClick}>
-      <ModalContainer ref={outside}>
-        <ModalHeader onClose={onClose} />
-        <Receiver />
-        <QuestionForm>
-          <textarea placeholder='질문을 입력하세요' />
-        </QuestionForm>
-      </ModalContainer>
-    </ModalWrapper>
+    <S.ModalWrapper onClick={handleWrapperClick}>
+      <S.ModalContainer ref={outside}>
+        <S.ModalHeader>
+          <S.Message>
+            <S.MessageIcon src={messageIcon} />
+            <S.Question>질문을 작성하세요</S.Question>
+          </S.Message>
+          <S.CloseIcon src={closeIcon} onClick={onClose} />
+        </S.ModalHeader>
+        <S.RecieverContainer>
+          <S.ProfleContainer>
+            <span>To.</span>
+            <S.ProfileIcon src={profileIcon} />
+            <span>아초는고양이</span>
+          </S.ProfleContainer>
+          <S.FormContainer>
+            <S.QuestionForm />
+            <S.Button>질문 보내기</S.Button>
+          </S.FormContainer>
+        </S.RecieverContainer>
+      </S.ModalContainer>
+    </S.ModalWrapper>
   );
 }
 
-const ModalHeader = ({ onClose }) => (
-  <HeaderContainer>
-    <ModalDescription />
-    <CloseButton onClick={onClose}>
-      <Icon src={CloseIcon} alt='닫기 버튼' />
-    </CloseButton>
-  </HeaderContainer>
-);
+const S = {};
 
-const Profile = () => <div>아초는고양이프로필컴포넌</div>;
-
-const Receiver = () => (
-  <ReceiverContainer>
-    <span>TO.</span>
-    <Profile />
-  </ReceiverContainer>
-);
-
-const ModalDescription = () => (
-  <DescriptionContainer>
-    <Icon src={MessageIcon} alt='메세지 아이콘' />
-    <span>질문을 입력해주세요</span>
-  </DescriptionContainer>
-);
-
-const ModalWrapper = styled.div`
+S.ModalWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100vw;
   height: 100vh;
   position: fixed;
@@ -59,72 +55,92 @@ const ModalWrapper = styled.div`
   background-color: rgba(0, 0, 0, 0.56);
 `;
 
-const Icon = styled.img`
-  width: 22.75px;
-  height: 22.75px;
-`;
-
-const CloseButton = styled.button`
-  width: 28px;
-  height: 28px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContainer = styled.div`
-  height: 568px;
-  gap: 20px;
-  border-radius: 24px 0px 0px 0px;
-
-  opacity: 1;
-  background: var(--Grayscale-10, #ffffff);
-  box-shadow: 0px 16px 20px 0px #3030309e;
-  padding: 20px;
-`;
-
-const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 279px;
-  position: absolute;
-  top: 146px;
-  left: 47px;
-`;
-
-const DescriptionContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 8px;
-`;
-
-const ReceiverContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 8px;
-  position: absolute;
-  top: 214px;
-  left: 47px;
-`;
-
-const QuestionForm = styled.form`
+S.ModalContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 8px;
-  width: 100%;
-  position: absolute;
-  top: 254px;
-  left: 47px;
+  align-items: center;
+  width: 327px;
+  height: 568px;
+  padding: 24px;
+  border-radius: ${({ theme }) => theme.rounded.lg};
+  background-color: ${({ theme }) => theme.grayScale.gray10};
+  gap: 40px;
 
-  textarea {
-    width: 279px;
-    height: 150px;
-    resize: none;
+  @media screen and (min-width: 768px) {
+    width: 612px;
+    height: 454px;
+    padding: 40px 40px 70px 40px;
   }
 `;
 
-export default Modal;
+S.ModalHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+S.Message = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+S.Question = styled.span`
+  font-family: ${({ theme }) => theme.font.family.second};
+  font-size: ${({ theme }) => theme.font.size.lg};
+  line-height: ${({ theme }) => theme.font.lineHeight.lg};
+  color: ${({ theme }) => theme.grayScale.gray60};
+`;
+
+S.RecieverContainer = styled.div`
+  width: 100%;
+`;
+
+S.ProfleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  margin-bottom: 12px;
+
+  span {
+    font-size: ${({ theme }) => theme.font.size.sm};
+    line-height: ${({ theme }) => theme.font.lineHeight.sm};
+    color: ${({ theme }) => theme.grayScale.gray60};
+  }
+`;
+
+S.FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+S.Button = styled(Button)`
+  font-size: ${({ theme }) => theme.font.size.sm};
+  padding: 12px;
+  height: 46px;
+`;
+
+S.QuestionForm = styled(QuestionForm)`
+  height: 358px;
+
+  @media screen and (min-width: 768px) {
+    height: 180px;
+  }
+`;
+
+S.MessageIcon = styled.img`
+  width: 28px;
+  height: 28px;
+`;
+
+S.CloseIcon = styled.img`
+  width: 22px;
+  height: 22px;
+`;
+
+S.ProfileIcon = styled.img`
+  width: 28px;
+  height: 28px;
+`;
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Form from './form';
 import styled from 'styled-components';
 
-export default function QuestionForm() {
+export default function QuestionForm({ className }) {
   const handleQuestionSubmit = (question) => {
     console.log('Question:', question);
   };
 
   return (
-    <QuestionFormWrapper>
+    <QuestionFormWrapper className={className}>
       <Form
         onSubmit={handleQuestionSubmit}
-        placeholder="질문을 입력해주세요"
+        placeholder='질문을 입력해주세요'
         showIcon={false}
         iconInstance={null}
         inputStyles={inputCustomStyles}
@@ -25,15 +25,11 @@ const QuestionFormWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 279px;
-  height: 358px;
   gap: 10px;
   border-radius: 10px;
 `;
 
 const inputCustomStyles = {
-  width: '247px',
-  height: '326px',
   padding: '10px',
   border: 'none',
   backgroundColor: '#f9f9f9',
